2d36cd7b87c44be397fda6260287b5d272ea4304
JoaoPedro-SA <antunesjoaopedro3@gmail.com>
2026-06-28 16:13:27 -0300

    erro do modelo beta

--- config.py
+++ config.py
@@ -13,4 +13,15 @@ RESERVA_API_BASE_URL = os.environ.get(
 def create_app():
     app = Flask(__name__)
     app.config['DEBUG'] = os.environ.get('FLASK_DEBUG', 'false').lower() == 'true'
+
+    @app.after_request
+    def add_cors_headers(response):
+        response.headers["Access-Control-Allow-Origin"] = os.environ.get(
+            "CORS_ALLOW_ORIGIN",
+            "*",
+        )
+        response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+        response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+        return response
+
     return app
